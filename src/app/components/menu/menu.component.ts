4236ce13e49c79742212687d90f972fc11169945
alberto.tsuna57 <alberto.tsuna57@gmail.com>
2025-09-24 01:02:44 +0000

    menu css

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -10,4 +10,9 @@ import { RouterModule } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
+  public isSearchVisible = signal(false);
+
+  public toggleSearch(): void {
+    this.isSearchVisible.set(!this.isSearchVisible());
+  }
 }
