152f001d89537f78103452bd9b88fa40dded5cb1
alberto.tsuna57 <alberto.tsuna57@gmail.com>
2025-10-30 23:09:02 +0000

    ajuste css

--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeHeaderComponent } from '../../components/home-header/home-header.component';
 
 @Component({
   selector: 'app-reset-password',
+  standalone: true,
+  imports: [CommonModule, HomeHeaderComponent],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
