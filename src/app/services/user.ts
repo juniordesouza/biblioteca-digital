e10b97a1ab4a964d3ea653eb3c6376babeec05e0
alberto.tsuna57 <alberto.tsuna57@gmail.com>
2025-11-11 00:50:50 +0000

    user service

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  username: string;
+  nomeCompleto: string;
+  email: string;
+  dtNasc: string;
+  cpf: string;
+  cep: string;
+  numero: string;
+  telefone: string;
+  imagemUrl?: string; // <- se vier a imagem
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class User {
-  
+export class UserService {
+  private apiUrl = 'https://api.seuservidor.com/usuarios'; // URL da sua API
+
+  constructor(private http: HttpClient) {}
+
+  getUsuarios(page: number = 1): Observable<{ data: User[]; totalPages: number }> {
+    return this.http.get<{ data: User[]; totalPages: number }>(`${this.apiUrl}?page=${page}`);
+  }
 }
