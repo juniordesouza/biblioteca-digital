3c9da75da02b56b4a98a84b134e2171ee9682186
alberto.tsuna57 <alberto.tsuna57@gmail.com>
2025-11-12 03:53:18 +0000

    ajuste homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HomeHeaderComponent } from '../../components/home-header/home-header.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MenuComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule, HomeHeaderComponent]
 })
 export class HomeComponent {
-
+  topBooks = [
+    { title: 'One Piece', image: 'assets/01.png' },
+    { title: 'A Arte da Guerra', image: 'assets/02.png' },
+    { title: 'Diário de um Banana', image: 'assets/03.png' },
+    { title: 'It - A Coisa', image: 'assets/04.png' },
+    { title: 'Duna', image: 'assets/05.png' }
+  ];
 }
